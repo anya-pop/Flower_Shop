d4ccb804f496d75d7768c0d325856eee6a74919b
anya-pop <114254783+anya-pop@users.noreply.github.com>
2023-10-14 14:14:10 -0400

    hardcoded data

---
+++ app/src/main/java/sheridan/popovann/assignment2/domain/Flower.kt
@@ -0,0 +1,9 @@
+package sheridan.popovann.assignment2.domain
+
+data class Flower (
+    val id: String,
+    val label: String,
+    val price: Double,
+    val description: String,
+    val image: String
+    )
