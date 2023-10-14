8e194e31cc708b9cc2efba55514922acfc346bdb
anya-pop <114254783+anya-pop@users.noreply.github.com>
2023-10-14 18:10:04 -0400

    added dependencies and strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "sheridan.popovann.assignment2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "sheridan.popovann.assignment2"
@@ -59,6 +59,14 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // needed for viewModel() function
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    // display images from web or assets
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
